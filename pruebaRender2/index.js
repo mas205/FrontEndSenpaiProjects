a41913893a3de5207cc44e89cc43847ed42dd124
mas205 <martinhargain@gmail.com>
2019-05-16 22:05:52 -0300

    Add files via upload

--- pruebaRender2/index.js
+++ pruebaRender2/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 var app = express();
-require('./router/main.js.js')(app);
+require('./router/main.js')(app);
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
+app.use('/css', express.static(__dirname + '/public/styles'));
 //app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/jquery', express.static(__dirname + '/jquery'));
 app.engine('html', require('ejs').renderFile);
